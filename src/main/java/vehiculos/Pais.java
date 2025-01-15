51876347969eadd76258f362de2c829a2576e910
meicon456 <jocastrore@unal.edu.co>
2025-01-14 21:25:23 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,34 +1,39 @@
 package vehiculos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pais {
     private String nombre;
     private int ventas;
+    private static List<Pais> paises = new ArrayList<>();
 
     public Pais(String nombre) {
         this.nombre = nombre;
+        paises.add(this);
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void registrarVenta() {
-        ventas++;
+    public void incrementarVentas() {
+        this.ventas++;
     }
 
     public int getVentas() {
         return ventas;
     }
 
-    public static Pais paisMasVendedor(Pais[] paises) {
-        Pais mayor = null;
-        int maxVentas = -1;
+    public static Pais paisMasVendedor() {
+        Pais masVendedor = null;
+        int maxVentas = 0;
         for (Pais p : paises) {
-            if (p.getVentas() > maxVentas) {
-                maxVentas = p.getVentas();
-                mayor = p;
+            if (p.ventas > maxVentas) {
+                maxVentas = p.ventas;
+                masVendedor = p;
             }
         }
-        return mayor;
+        return masVendedor;
     }
 }
